29ee3cfe4de88afe959fe78d8f9293e7e1b34909
Gabriel Ribeiro <gabriel.ribalves@gmail.com>
2023-10-03 10:48:58 -0300

    Adding tests

--- tests/test_load.py
+++ tests/test_load.py
@@ -6,5 +6,16 @@ from .constants import PEOPLE_FILE
 @pytest.mark.high
 def test_load():
     """Test load function """
+    with open(f"arquivo_indesejado.txt", "w") as file_:
+        file_.write("dados uteis somente para teste")
+    assert len(load(PEOPLE_FILE)) == 2
+    assert load(PEOPLE_FILE)[0][0] == 'J'
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load2():
+    """Test load function """
+    with open(f"arquivo_indesejado.txt", "w") as file_:
+        file_.write("dados uteis somente para teste")
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == 'J'
